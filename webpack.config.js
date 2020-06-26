18cb5e0b161759796374c9ab9112de8a1f5ab434
CMH <18814008467@163.com>
2020-06-26 12:22:58 +0800

    '加好注释'

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path');
+// 从bundle中提取CSS到单独的文件
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+// 简单创建HTML文件，用于服务器访问
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development', // development模式打包出来的文件是未压缩的 production模式打包出来的文件是压缩的
-    entry: ['./app/index.js', './app/addImage.js'],
+    entry: ['./app/index.js', './app/addImage.js'], // 向entry属性传入（文件路径数组）将创建“多个主入口”。当你想要多个依赖文件一起注入，并且将他们的依赖导向到一个chunk时候，传入数组的方式就很有用
     output: {
-        filename: 'bundle.js', // 打包后删除文件的文件名
-        path: path.resolve(__dirname, 'build'), // 必须使用绝对地址，输出文件夹
-        publicPath: './' // 知道如何寻找资源
+        filename: 'bundle.js', // 打包后输出文件的文件名
+        path: path.resolve(__dirname, 'build'), // 使用绝对地址，输出文件夹 到 /buiild 下
+        publicPath: './' // 输出解析文件的目录 url相对于打包后的HTML页面
     },
     module: {
         rules: [
@@ -16,20 +18,21 @@ module.exports = {
             {
                 test: /\.js$/, // js文件才使用babel
                 use: 'babel-loader', // 使用哪个loader
-                exclude: /node_modules/ // 不包括路径
+                exclude: /node_modules/ // 不转译node_modules目录 这样babel-loader快些
             },
-            // 图片格式正则
+            // 图片
             {
                 test: /\.(jpg|png|gif|bmp|jpeg)$/,
                 use: [
                     {
-                        loader: 'url-loader', // 配置url-loader的可选项
+                        loader: 'url-loader', 
+                        // 配置url-loader的可选项
                         options: {
-                            esModule: false,
+                            // 不生成使用ES模块语法的JS模块
+                            esModule: false, 
                             // 限制图片大小10000B 小于限制会将图片转换为base64格式
                             limit: 1000, 
                             // 超出限制 创建的文件格式
-                            // build/images/[图片名].[hash].[图片格式]
                             name: 'images/[name].[hash].[ext]'
                         }
                     }
@@ -52,10 +55,13 @@ module.exports = {
             {
                 test: /\.css$/,
                 loader: ExtractTextPlugin.extract({
+                    // 当CSS没有被提取
                     fallback: 'style-loader',
                     use: [{
+                        // 将资源转换成一个CSS模块导出
                         loader: 'css-loader',
                         options: {
+                            // 启动CSS模块
                             modules: true
                         }
                     }]
@@ -67,6 +73,7 @@ module.exports = {
     plugins: [
         // 输出的文件路径
         new ExtractTextPlugin("css/[name].[hash].css"),
+        // 
         new HtmlWebpackPlugin({
             template: './index.html',
             filename: 'test.html',
